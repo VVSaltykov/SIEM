67457c0b37999f2c8402a84175c39687f575da6a
SaltykovVV <SaltykovVV@OIT-ZAMENA-04>
2026-07-14 12:26:52 +0400

    Add project files.

---
+++ SIEM.LogCollector.Host/Program.cs
@@ -0,0 +1,14 @@
+namespace SIEM.LogCollector.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
